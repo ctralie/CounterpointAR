7c06df619de20656594185441fec79c2bc2f4475
mtecce <mitecce@ursinus.edu>
2021-09-23 11:05:06 -0400

    Trying different patterns

--- posar.js
+++ posar.js
@@ -98,7 +98,7 @@ class PositionalAR {
 
         let markerParameters = {
             type: "pattern",
-            patternUrl: "data/letterB.patt",
+            patternUrl: "data/kanjii.patt",
             // turn on/off camera smoothing
             smooth: true,
             // number of matrices to smooth tracking over, more = smoother but slower follow
