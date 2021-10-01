7dde1a6b317ff4ea9c6a1e55bdcf52dd558997f9
mtecce <mitecce@ursinus.edu>
2021-09-30 22:21:11 -0400

    multiple trackers average working

--- posar.js
+++ posar.js
@@ -89,47 +89,84 @@ class PositionalAR {
         this.arToolkitContext = arToolkitContext;
 
         // copy projection matrix to camera when initialization complete
-        arToolkitContext.init();//(function onCompleted(){
-        //    that.camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
-        //});
+        arToolkitContext.init(function onCompleted(){
+            that.camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
+        });
 
-        // Adding the AR markerControls
-        const patterns = ["data/hiro.patt", "data/kanji.patt", "data/letterA.patt", "data/letterB.patt", "data/letterC.patt", "data/letterD.patt", "data/letterF.patt", "data/letterG.patt"];
-        let markerRoots = [];
-        let markersVisible = [];
-        let lastMarkerPos = []; // For keeping track of the last marker positions
-        this.markerRoots = markerRoots;
-        this.markersVisible = markersVisible;
-        this.lastMarkerPos = lastMarkerPos;
-        for (let i = 0; i < patterns.length; i++) {
-            const markerRoot = new THREE.Group();
-            this.scene.add(markerRoot);
-            markerRoots.push(markerRoot);
-            const markerControl = new THREEx.ArMarkerControls(arToolkitContext, markerRoot, {
-                type: 'pattern', patternUrl: patterns[i],
-            });
-            markerControl.i = i;
-            markerControl.addEventListener("markerFound", (e)=>{
-                that.markersVisible[e.target.i] = true;
-                console.log("marker found");
-            });
-            markerControl.addEventListener("markerLost", (e)=>{
-                markersVisible[e.target.i] = false;
-                console.log("marker lost");
-            });
-            markersVisible.push(false);
-            lastMarkerPos.push([0, 0, 0]);
-        }
+        const markerRoot = new THREE.Group();
+        this.markerRoot = markerRoot;
+        this.scene.add(markerRoot);
+
+        let markerParameters = {
+            type: "pattern",
+            patternUrl: "data/letterA.patt",
+            // turn on/off camera smoothing
+            smooth: true,
+            // number of matrices to smooth tracking over, more = smoother but slower follow
+            smoothCount: 5,
+            // distance tolerance for smoothing, if smoothThreshold # of matrices are under tolerance, tracking will stay still
+            smoothTolerance: 0.01,
+            // threshold for smoothing, will keep still unless enough matrices are over tolerance
+            smoothThreshold: 2
+        };
+
+        
+
+        const markerControl = new THREEx.ArMarkerControls(arToolkitContext, markerRoot, markerParameters);
+        markerControl.addEventListener("markerFound", (e)=>{
+            // TODO: We can do stuff once a marker is found
+            console.log("Found");
+        });
+        markerControl.addEventListener("markerLost", (e)=>{
+            // TODO: We can do stuff once a marker is lost
+            console.log("lost");
+        });
+
+        
+        
+        // Tether the scene to this marker
+        // TODO: We will want to involve multiple markers somehow
+        //markerRoot.add(this.sceneRoot);
+
+        const markerRoot1 = new THREE.Group();
+        this.markerRoot1 = markerRoot1;
+        this.scene.add(markerRoot1);
+
+        let markerParameters1 = {
+            type: "pattern",
+            patternUrl: "data/kanji.patt",
+            // turn on/off camera smoothing
+            smooth: true,
+            // number of matrices to smooth tracking over, more = smoother but slower follow
+            smoothCount: 5,
+            // distance tolerance for smoothing, if smoothThreshold # of matrices are under tolerance, tracking will stay still
+            smoothTolerance: 0.01,
+            // threshold for smoothing, will keep still unless enough matrices are over tolerance
+            smoothThreshold: 2
+        };
+
+        const markerControl1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, markerParameters1);
+        markerControl1.addEventListener("markerFound", (e)=>{
+            // TODO: We can do stuff once a marker is found
+            console.log("Found");
+        });
+        markerControl1.addEventListener("markerLost", (e)=>{
+            // TODO: We can do stuff once a marker is lost
+            console.log("lost");
+        });
+
+        //markerRoot1.add(this.sceneRoot);
         this.arGroup = new THREE.Group();
         this.arGroup.add(this.sceneRoot);
         this.scene.add(this.arGroup);
+        let markerRoots = [markerRoot, markerRoot1];
+        this.markerRoots = markerRoots;
     }
 
     /**
      * Perform an animation step, which consists of tracking the AR targets and updating
      * the global AR positions, as well as animating the scene forward in time
      */
-    /*
     repaint() {
         if ( this.arToolkitSource.initialized !== false ) {
             this.arToolkitContext.update( this.arToolkitSource.domElement );
@@ -150,101 +187,15 @@ class PositionalAR {
                 this.markerRoot.position.y -= K.moveud*K.walkspeed*delta/1000;
                 this.markerRoot.position.z += K.movefb*K.walkspeed*delta/1000;
             }
-        }
-        this.renderer.render(this.scene, this.camera);
-        requestAnimationFrame(this.repaint.bind(this));
-    }
-    */
-    repaint() {
-        if ( this.arToolkitSource.ready !== false ) {
-            this.arToolkitContext.update( this.arToolkitSource.domElement );
-            this.arToolkitSource.domElement.display = "none";
-        }
-        let deltaTime = this.clock.getDelta();
-        if (this.totalTime < 6 && (this.totalTime+deltaTime)%1 < this.totalTime%1) {
-            // A hack to trigger resizing every second for the first 5 seconds
-            // TODO: Try something more elegant?
-            this.onResize();
-        }
-        this.totalTime += deltaTime;
-        this.sceneObj.animate(deltaTime);
-
-        const arGroup = this.arGroup;
-        if (this.keyboardDebugging) {
-            const K = this.keyboard;
-            if (K.movelr != 0 || K.moveud != 0 || K.movefb != 0) {
-                arGroup.position.x -= K.movelr*K.walkspeed*delta/1000;
-                arGroup.position.y -= K.moveud*K.walkspeed*delta/1000;
-                arGroup.position.z += K.movefb*K.walkspeed*delta/1000;
-            }
-        }
-        else {
-            // Use markers
+        }else{
             const markerRoots = this.markerRoots;
-            const markersVisible = this.markersVisible;
-            const lastMarkerPos = this.lastMarkerPos;
-            let dx = 0;
-            let dy = 0;
-            let dz = 0;
-            let count = 0;
-            // Step 1: Average the changes in position of the visible markers
-            for (let i = 0; i < markerRoots.length; i++) {
-                if (markersVisible[i]) {
-                    dx += markerRoots[i].position.x - lastMarkerPos[i][0];
-                    dy += markerRoots[i].position.y - lastMarkerPos[i][1];
-                    dz += markerRoots[i].position.z - lastMarkerPos[i][2];
-                    count += 1;
-                    lastMarkerPos[i][0] = markerRoots[i].position.x;
-                    lastMarkerPos[i][1] = markerRoots[i].position.y;
-                    lastMarkerPos[i][2] = markerRoots[i].position.z;
-                }
-            }
-            if (count > 0) {
-                // Step 2: Apply this change to the unseen markers and add up all
-                // of the positions
-                let x = 0;
-                let y = 0;
-                let z = 0;
-                for (let i = 0; i < markerRoots.length; i++) {
-                    if (!markersVisible[i]) {
-                        lastMarkerPos[i][0] += dx/count;
-                        lastMarkerPos[i][1] += dy/count;
-                        lastMarkerPos[i][2] += dz/count;
-                    }
-                    x += lastMarkerPos[i][0];
-                    y += lastMarkerPos[i][1];
-                    z += lastMarkerPos[i][2];
-                    markersVisible[i] = false;
-                }
-                // Step 3: Set the position to be the average
-                arGroup.position.x = x/markerRoots.length;
-                arGroup.position.y = y/markerRoots.length;
-                arGroup.position.z = z/markerRoots.length;
-                if (this.debug) {
-                    console.log(count + " markers seen");
-                    console.log("x = " + arGroup.position.x + ", y = " + arGroup.position.y + ", z = " + arGroup.position.z);
-                }
-                //arGroup.rotation.x = markerRoot1.rotation.x;
-                //arGroup.rotation.y = markerRoot1.rotation.y;
-                //arGroup.rotation.z = markerRoot1.rotation.z;
-    
-                arGroup.rotation.x = 0;
-                arGroup.rotation.y = 0;
-                arGroup.rotation.z = 0;
-            }
-            else {
-                if (this.debug) {
-                    console.log("No markers found");
-                }
-            }
+            this.arGroup.position.x = (markerRoots[1].position.x + markerRoots[0].position.x)/2;
+            this.arGroup.position.y = (markerRoots[1].position.y + markerRoots[0].position.y)/2;
+            this.arGroup.position.z = (markerRoots[1].position.z + markerRoots[0].position.z)/2;
+            console.log(markerRoots[0].position);
+            console.log(this.arGroup.position);
         }
-        // Update VR headset position and apply to camera.
-        //controls.update();
-
-        // Render the scene.
         this.renderer.render(this.scene, this.camera);
-
-        // Keep looping.
         requestAnimationFrame(this.repaint.bind(this));
     }
 
