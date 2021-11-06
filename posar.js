6d950d01018fee38729f2faec418bffadc59aeed
mtecce <60228508+mtecce@users.noreply.github.com>
2021-11-06 12:12:39 -0400

    detection mode updated

--- posar.js
+++ posar.js
@@ -107,8 +107,8 @@ class PositionalAR {
         // create arToolkitContext
         const arToolkitContext = new THREEx.ArToolkitContext({
             cameraParametersUrl: 'data/camera_para.dat',
-            detectionMode: 'mono',
-            matrixCodeType: '4x4_BCH_13_5_5'
+            detectionMode: 'mono_and_matrix',
+            matrixCodeType: '4x4_BCH_13_9_3'
         });
         this.arToolkitContext = arToolkitContext;
 
