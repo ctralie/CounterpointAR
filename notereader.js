adedc4426702ab3593d06cb7978d37a08f227502
mtecce <60228508+mtecce@users.noreply.github.com>
2022-02-07 19:40:54 -0500

    piano roll inut functional, need to map input roll to data structure of audio class

--- notereader.js
+++ notereader.js
@@ -4,6 +4,10 @@ class NoteReader {
         this.fileReady = false;
         this.data = null; // Reference to promise
         this.lines = [];
+        this.timeSig = [];
+        this.clef = "";
+        this.cantusFirmusNotes = [];
+        this.counterpointNotes = [];
     }
 
     loadFile(filename) {
@@ -15,7 +19,7 @@ class NoteReader {
                 $.get(filename, function(s) {
                     let lines = s.split("\n");
                     for (let i = 0; i < lines.length; i++) {
-                        if (lines[i][0] != "#") {
+                        if ((lines[i][0] != "@") && (lines[i] != "")) {
                             that.lines.push(lines[i]);
                         }
                     }
@@ -28,5 +32,49 @@ class NoteReader {
             }
         );
         return this.data;
+    }
+
+    /*
+    Time signature: (2,2)
+    Clef: (Treble)
+    Cantus Firmus:
+    d : 2
+    ...
+    Counterpoint:
+    d : 2
+    ...
+    */
+    parseInfo(){
+        console.log(this.lines)
+        let timeSigIndex = 0;
+        let clefIndex = 1;
+        let cantFirmIndex = 3;
+        let cPointIndex = 0;
+        for(let i = 0; i < this.lines.length; i++){
+            if(this.lines[i].includes("Counterpoint:")){
+                cPointIndex = i+1;
+                i = this.lines.length;
+            }
+        }
+        let indArr = [timeSigIndex,clefIndex];
+        for(let i = 0; i < indArr.length; i++){
+            let index = this.lines[indArr[i]].indexOf(":");
+            let info = this.lines[indArr[i]].substring(index);
+            if(i == 0){
+                //time sig
+                info = info.substring(info.indexOf("(")+1);
+                this.timeSig.push(parseInt(info.substring(0,1)));
+                this.timeSig.push(parseInt(info.substring(2,3)));
+                console.log(this.timeSig);
+            }else{
+                //clef
+                this.clef = info.substring(info.indexOf("(")+1,info.indexOf(")"));
+                console.log(this.clef);
+            }
+        }
+        //cantus firmus
+
+        //counterpoint
+        
     }
 }
